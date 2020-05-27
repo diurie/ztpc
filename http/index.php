3fe1088a963b34e0977dcfd93ae697b0ae4a13ff
Didier Urie <urie.didier@gmail.com>
2020-05-27 12:08:34 +0200

    Initialization

---
+++ http/index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>ZTPc</title>
+</head>
+<body>
+
+<?php
+echo "<h1>Welcome to ZTPc Project</h1>";
+echo "<p>This is a paragraph where there will be content</p>";
+?>
+
+</body>
+</html>
